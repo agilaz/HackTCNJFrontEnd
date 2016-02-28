351b306e6c2a7f092e4c3499e824763c7f8618e1
HDell <tue73811@temple.edu>
2016-02-27 21:06:39 -0500

    Conflict resolution

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,28 +5,24 @@ alert("LKJKLFJ:LKJSLD:FJS:J");
 var wantedCourse = {subject:"A" , courseNumber: "B" , name: "C" , day:"D" , attribute: "[A,B]"}
 var dbCourse = {subject:"A" , courseNumber: "B" , name: "C" , day:"D" , attribute: "[A,B]"}
 
-	function isSubset(wantedCourse.attribute , dbCourse.attribute) {
-	var i = 0;
-	var j = 0;
-	var m = (wantedCourse.attribute.length) / (wantedCourse.attribute[0].length);
-	var n = (dbCourse.attribute.length) / (dbCourse.attribute[0].length);
+function isSubset(wantedCourse.attribute , dbCourse.attribute) {
+var m = (wantedCourse.attribute.length) / (wantedCourse.attribute[0].length);
+var n = (dbCourse.attribute.length) / (dbCourse.attribute[0].length);
 
-	for (i=0; i<n; i++) {
-		for (j = 0; j<m; j++) {
+for (i=0; i<n; i++) {
+	for (j = 0; j<m; j++) {
 
-			
-		if(arr2[i] == arr1[j]) {
-				break;
-			}
+		
+	if(arr2[i] == arr1[j]) {
+			break;
 		}
-		if (j==m) {
-				return 0;
-			}
+	}
+	if (j==m) {
+			return 0;
 		}
-
-		return 1;
-
-	}	
+	}
+	return 1;
+}	
 
 function filter(wantedCourse , dbCourse ) {
 	if (dbCourse.subject != wantedCourse.subject) {
@@ -44,9 +40,9 @@ function filter(wantedCourse , dbCourse ) {
 	if ( isSubset(wantedCourse.attribute , dbCourse.attribute) != 1) {
 		return false;
 	}
+<<<<<<< Updated upstream
 	return true; 
 }
-
 	
 	if (filter(wantedCourse , dbCourse)) {
 		alert("The filter is true");
