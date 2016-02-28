e97ecec2e2f93f66ea0f9d258291332e81992df0
van <georgekeenan@gmail.com>
2016-02-28 05:48:50 -0500

    It is 5:48AM we did something

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,46 @@
 //console.log('This would be the main JS file.');
 //filler
 
-var wantedCourse = {subject:"A" , courseNumber: "B" , name: "C" , day:"D" , attribute: "[A,B]"}
-var dbCourse = {subject:"A" , courseNumber: "B" , name: "C" , day:"D" , attribute: "[A]"}
+var wantedCourse = {subject:"A" , courseNumber: "B" , name: "C" , day:"[D]" , attribute: "[A,B]"}
+var dbCourse = {subject:"AAS" , courseNumber: "B" , name: "C" , day:"[D]" , attribute: "[A,B,C]"}
 
-function filter(wantedCourse , dbCourse ) {
+//Get the name of the course
+function setSubject(){
+var nameList = document.getElementById("SSR_CLSRCH_WRK_SUBJECT_SRCH$0");
+document.getElementById("render1").value = nameList.options[nameList.selectedIndex].value;
+var x = render1.value;
+return x;
+}
+
+
+//function testAlert() {
+//	alert(wantedCorse.subject);
+//}
+//function courseName(){
+//wantedCourse.name = courseName.value;
+//}
+
+function setProperties(){
+	wantedCourse.subject = displayChoice(); //wantedCourse.subject = the value of what the user selects
+	alert(wantedCourse.subject); 
+}
+	
+
+
+
+	//value of selected
+//wantedCourse.subject = render1.value;
+//wantedCourse.day = [document.getElementById("MON").getAttribute("value"),document.getElementById("TUE").getAttribute("value"),document.getElementById("WED").getAttribute("value"),document.getElementById("THUR").getAttribute("value"),document.getElementById("FRI").getAttribute("value"),document.getElementById("SAT").getAttribute("value"),document.getElementById("SUN").getAttribute("value")]
+//alert(watchCourse.day[0]);
+//alert(wantedCourse.subject);
+//	if (render1.value == "") {
+//		alert("sa;ldfkj
+//}
+
+function filter(wantedCourse , dbCourse ) {	
 	if (dbCourse.subject != wantedCourse.subject) {
 		return false;
-	}
+	} 
 	if (dbCourse.courseNumber != wantedCourse.courseNumber) {
 		return false;
 	}
@@ -38,22 +71,8 @@ function filter(wantedCourse , dbCourse ) {
 		}//end of if (64)
 		i++;
 	}//end of while 57
-	return true; 
-}
-	
-if (filter(wantedCourse , dbCourse)) {
-	alert("The filter is true");
-}
-else {
-	alert("The filter is false");
+	alert("trueeee")
+	return true;
 }
 
-//Get the name of the course
-function displayChoice(){
-var nameList = document.getElementById("SSR_CLSRCH_WRK_SUBJECT_SRCH$0");
-var something = document.getElementById("render1").value=nameList.options[nameList.selectedIndex].value;
-}
-
-function testAlert(){
-	alert(nameList.);
-}
+filter(wantedCourse , dbCourse);
